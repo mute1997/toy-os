db8ed1044d402f8b702bc315c4b50745fcad10f6
mute1997 <mute1008@gmail.com>
2018-04-18 09:51:12 +0900

    [WIP] add printk

--- kernel/printk.c
+++ kernel/printk.c
@@ -1,11 +1,18 @@
 #include <drivers/video.h>
 #include <std/stdarg.h>
 
-// static char printk_buf[1024];
+#define LOG_BUF_SIZE 32768
+
+static char printk_buf[LOG_BUF_SIZE][1024];
+// static unsigned long top;
+static unsigned long bottom;
+
 extern int vsnprintf(char *buf, unsigned long size, const char* fmt, va_list arg);
 
 int vprintk(const char *fmt, va_list args) {
-  int printed_len = 0;
+  // TODO: ここで落ちてるくさい
+  int printed_len = vsnprintf(printk_buf[bottom], sizeof(printk_buf[bottom]), fmt, args);
+
   flush_screen();
 
   // TODO: printk_bufの内容を書き出し
--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -2,4 +2,5 @@
 
 int vsnprintf(char *buf, unsigned long size, const char* fmt, va_list arg) {
   // TODO: bufに書き込む
+  return 0;
 }
