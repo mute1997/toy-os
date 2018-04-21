48ec26beaf0c7ec5547311007a7a104067b41302
mute1997 <mute1008@gmail.com>
2018-04-21 09:33:59 +0900

    [WIP] add gdt

--- arch/i386/boot/pm.c
+++ arch/i386/boot/pm.c
@@ -1,21 +1,34 @@
 #include <asm/types.h>
 #include <std/printk.h>
 
-/* GDT pointer */
-struct gdt_ptr {
-  u16 len;
-  u32 ptr;
-} __attribute__ ((packed));
+#define GDT_ENTRIES 7
 
-/* GDT Entry */
-struct gdt_entry {
-  u16 limit0;
-  u16 base0;
-  u16 base1: 8, type: 4, s: 1, dpl: 2, p: 1;
-  u16 limit1: 4, avl: 1, l: 1, d: 1, g: 1, base2: 8;
-} __attribute__ ((packed));
+struct desc_struct {
+  union {
+    /* Segment pointer */
+    struct {
+      u16 len;
+      u32 ptr;
+    };
 
-void gdt_entry_init(u8 flags, u16 base, u16 limit) {
+    /* Segment Descriptor */
+    struct {
+      u16 limit0;
+      u16 base0;
+      u16 base1: 8, type: 4, s: 1, dpl: 2, p: 1;
+      u16 limit1: 4, avl: 1, l: 1, d: 1, g: 1, base2: 8;
+    };
+  };
+} __attribute__((packed));
+
+/* GDT */
+struct gdt_page {
+  struct desc_struct gdt[GDT_ENTRIES];
+};
+
+static struct gdt_page gdt;
+
+void gdt_entry_init(u8 flags, u16 base, u16 limit, struct gdt_page *desc) {
   /* TODO: */
 }
 
