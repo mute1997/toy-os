9c7069fe19b5a46a4bceca1fc6fa1a9591518594
mute1997 <mute1008@gmail.com>
2018-04-21 11:44:16 +0900

    add gdt

--- arch/i386/boot/pm.c
+++ arch/i386/boot/pm.c
@@ -3,22 +3,16 @@
 
 #define GDT_ENTRIES 7
 
-struct desc_struct {
-  union {
-    /* Segment pointer */
-    struct {
-      u16 len;
-      u32 ptr;
-    };
+struct gdt_ptr {
+  u16 len;
+  u32 ptr;
+};
 
-    /* Segment Descriptor */
-    struct {
-      u16 limit0;
-      u16 base0;
-      u16 base1: 8, type: 4, s: 1, dpl: 2, p: 1;
-      u16 limit1: 4, avl: 1, l: 1, d: 1, g: 1, base2: 8;
-    };
-  };
+struct desc_struct {
+  u16 limit0;
+  u16 base0;
+  u16 base1: 8, type: 4, s: 1, dpl: 2, p: 1;
+  u16 limit1: 4, avl: 1, l: 1, d: 1, g: 1, base2: 8;
 } __attribute__((packed));
 
 /* GDT */
@@ -26,13 +20,31 @@ struct gdt_page {
   struct desc_struct gdt[GDT_ENTRIES];
 };
 
-static struct gdt_page gdt;
+static struct gdt_ptr kgdt_ptr;
+static struct gdt_page kgdt;
 
-void gdt_entry_init(u8 flags, u16 base, u16 limit, struct gdt_page *desc) {
-  /* TODO: */
+void gdt_entry_init(u8 flags, u16 base, u16 limit, struct desc_struct *desc) {
+  desc->limit0 = limit;
+  desc->limit1 = ((limit) >> 16) & 0x0F;
+  desc->base0 = base;
+  desc->base1 = ((base) >> 16) & 0xFF;
+  desc->base2 = ((base) >> 24) & 0xFF;
+  desc->type = flags & 0x0F;
+  desc->s = (flags >> 4) & 0x01;
+  desc->dpl = (flags >> 5) & 0x03;
+  desc->p = (flags >> 7) & 0x01;
+  desc->avl = (flags >> 12) & 0x01;
+  desc->l = (flags >> 13) & 0x01;
+  desc->d = (flags >> 14) & 0x01;
+  desc->g = (flags >> 15) & 0x01;
 }
 
+/* TODO: */
 void setup_gdt() {
-  /* TODO: */
   printk("Setup gdt...");
+
+  /* Init gdt segments */
+  gdt_entry_init(0x0, 0x0, 0x0, &kgdt.gdt[0]);
+
+  printk("[OK]");
 }
