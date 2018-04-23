14857e695568e661118948f672e91f0a7c7851ed
mute1997 <mute1008@gmail.com>
2018-04-23 16:20:18 +0900

    add GDT table

--- arch/i386/boot/protect.c
+++ arch/i386/boot/protect.c
@@ -1,53 +1,61 @@
 #include <asm/types.h>
 #include <std/printk.h>
+#include <std/string.h>
 #include <archtypes.h>
+
+#define load_gdt(dtr) __asm__ __volatile__ ("lgdt %0"::"m"(dtr))
+#define init_seg() __asm__ __volatile__ ("\n \
+    mov %ax,%ds \n \
+    mov %ax,%es \n \
+    mov %ax,%fs \n \
+    mov %ax,%gs \n \
+    mov %ax,%ss \n \
+    ")
 
 static struct gdt_ptr kgdt_ptr;
 static struct gdt_page kgdt;
 
 /* Init Segment descriptor */
 void init_segment(struct desc_struct *desc, u32 base, u32 size) {
-  /* TODO: マジックナンバーが多いので修正する */
   desc->base_low = base;
   desc->base_middle = base >> 16;
   desc->base_high = base >> 24;
-
-  /* TODO limit_low */
-  // desc->limit_low = 0;
-
-  /* TODO granularity */
-  // desc->granularity = 0;
+  desc->limit_low = size >> 12;
+  desc->granularity =  GRANULAR | (size >> (PAGE_GRAN_SHIFT + GRANULARITY_SHIFT)) | DEFAULT;
 }
 
 void init_data_segment(struct desc_struct *desc, u32 base, u32 size, int privilege) {
   init_segment(desc, base, size);
-  /* TODO: access byteの設定*/
+  desc->access = (privilege << 5) | (PRESENT | SEGMENT | WRITABLE);
 }
 
 void init_code_segment(struct desc_struct *desc, u32 base, u32 size, int privilege) {
   init_segment(desc, base, size);
-  /* TODO: access byteの設定*/
+  desc->access = (privilege << 5) | (PRESENT | SEGMENT | EXECUTABLE | READABLE);
 }
 
 void switch_to_new_gdt() {
+  load_gdt(kgdt_ptr);
+  init_seg();
 }
 
 void setup_gdt() {
   printk("Setup gdt...");
 
   /* Init GDT */
-  // memset(kgdt, 0, sizeof(kgdt));
+  memset(kgdt.gdt, 0, sizeof(kgdt));
 
   /* set GDT pointer */
   kgdt_ptr.base = (u32)kgdt.gdt;
   kgdt_ptr.limit = sizeof(kgdt.gdt)-1;
 
   /* Init GDT segments */
-  /* TODO: unusable LDT */
-  init_code_segment(&kgdt.gdt[0], 0x0, 0x0, KERNEL_PRIVILEGE); /* kernel code */
-  init_data_segment(&kgdt.gdt[0], 0x0, 0x0, KERNEL_PRIVILEGE); /* kernel data */
-  init_code_segment(&kgdt.gdt[0], 0x0, 0x0, USER_PRIVILEGE); /* user code */
-  init_data_segment(&kgdt.gdt[0], 0x0, 0x0, USER_PRIVILEGE);  /* user data */
+  init_code_segment(&kgdt.gdt[0], 0, 0xFFFFFFFF, KERNEL_PRIVILEGE); /* kernel code */
+  init_data_segment(&kgdt.gdt[1], 0, 0xFFFFFFFF, KERNEL_PRIVILEGE); /* kernel data */
+  init_code_segment(&kgdt.gdt[2], 0, 0xFFFFFFFF, USER_PRIVILEGE); /* user code */
+  init_data_segment(&kgdt.gdt[3], 0, 0xFFFFFFFF, USER_PRIVILEGE);  /* user data */
+  init_segment(&kgdt.gdt[4], 0x0, 0); /* unusable LDT */
+  kgdt.gdt[4].access = PRESENT | 2;
 
   /* set new GDT */
   switch_to_new_gdt();
--- arch/i386/include/archtypes.h
+++ arch/i386/include/archtypes.h
@@ -2,6 +2,22 @@
 
 #define GDT_ENTRIES 7
 
+#define GRANULARITY_SHIFT 16
+#define PAGE_GRAN_SHIFT 12
+#define DEFAULT 0x40
+
+/* GDT access byte */
+#define PRESENT 0x80
+#define SEGMENT 0x10
+#define EXECUTABLE 0x08
+#define DIRECTION 0x04
+#define READABLE 0x02
+#define WRITABLE 0x02
+#define ACCESS 0x01
+
+/* GDT Granularity byre */
+#define GRANULAR 0x80
+
 /* GDT table pointer */
 struct gdt_ptr {
   u16 limit;
