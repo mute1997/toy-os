4268a6e7c7b91e2bf520aa8d3fb0a6574c5be74f
mute1997 <mute1008@gmail.com>
2018-05-05 17:58:29 +0900

    fix outb function

--- include/asm/io.h
+++ include/asm/io.h
@@ -1,11 +1,11 @@
 #include <asm/types.h>
 
 static inline void outb(u8 val, u16 port) {
-	__asm__ __volatile__ ("outb %0,%1" : : "a" (val), "dN" (port));
+	__asm__ __volatile__ ("outb %0,%1" : : "a" (val), "Nd" (port));
 }
 
 static inline u8 inb(u16 port) {
   u8 ret;
-  __asm__ __volatile__("inb %1,%0" : "=a" (ret) : "d" (port));
+  __asm__ __volatile__("inb %1,%0" : "=a" (ret) : "Nd" (port));
   return ret;
 }
