e5c075e3d6a26e4a969b1ac2c50384335f0704e2
mute1997 <mute1008@gmail.com>
2018-05-11 14:35:57 +0900

    fix run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 if uname | grep Darwin > /dev/null ; then
-  make osx
-  make qemu
+  make osx && make qemu
 elif uname | grep Linux > /dev/null ; then
-  make
-  make image
-  make qemu
+  make && make image && make qemu
 fi
