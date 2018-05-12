903a010d7ba65ef16c4f57adfadd5536d9587149
mute1997 <mute1008@gmail.com>
2018-05-12 11:57:01 +0900

    add multiboot header

--- arch/i386/kernel/setup.c
+++ arch/i386/kernel/setup.c
@@ -5,6 +5,7 @@
 #include <asm/types.h>
 #include <proto.h>
 #include <trap.h>
+#include <grub/multiboot2.h>
 
 extern unsigned long _bss_start;
 extern unsigned long _bss_end;
@@ -14,12 +15,23 @@ void setup_memory() {
   memset((void *)_bss_start, 0x00, _bss_end-_bss_start);
 }
 
-void setup() {
+void setup(unsigned long magic, unsigned long addr) {
+  struct multiboot_tag *tag;
+
   setup_memory();
   flush_screen();
 
+  if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) printk("invalid magic number");
+
   intr_init(); /* Initialize PIC */
   prot_init(); /* Initialize GDT, IDT, trap */
+
+  for (tag = (struct multiboot_tag *) (addr + 8);
+       tag->type != MULTIBOOT_TAG_TYPE_END;
+       tag = (struct multiboot_tag *) ((multiboot_uint8_t *) tag 
+				       + ((tag->size + 7) & ~7))) {
+    // printk("Memory lower: %d",((struct multiboot_tag_basic_meminfo *) tag)->mem_lower)
+  }
 
   // software interrupt
   __asm__ __volatile__ ("int3");
