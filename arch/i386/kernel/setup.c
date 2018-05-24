cbec986b0da8ace54017803136dfda88a6bd1ebc
mute1997 <mute1008@gmail.com>
2018-05-24 10:56:18 +0900

    work higherhalf

--- arch/i386/kernel/setup.c
+++ arch/i386/kernel/setup.c
@@ -11,13 +11,13 @@
 void setup(u32 magic, u32 addr) {
   if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) printk("invalid magic number");
   if (addr & 7) printk("unaligned mbi");
-
+  
   setup_heap();
   flush_screen();
-
+  
   intr_init(); /* Initialize PIC */
-  prot_init(); /* Initialize GDT, IDT, trap */
-  setup_physical_memory(addr); /* fill 1 to physical memory */
-
+  // prot_init(); /* Initialize GDT, IDT, trap */
+  // setup_physical_memory(addr); /* fill 1 to physical memory */
+  
   hlt();
 }
