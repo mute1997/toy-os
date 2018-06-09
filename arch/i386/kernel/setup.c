7425bff9c8de21002be7dc8b2d3594413fab98d2
mute1997 <mute1008@gmail.com>
2018-06-09 15:53:56 +0900

    Revert "add function for setup paging"
    This reverts commit 13c708089c77d1d1c06ba129631e36ffd9471e6b.

--- arch/i386/kernel/setup.c
+++ arch/i386/kernel/setup.c
@@ -8,20 +8,12 @@
 #include <grub/multiboot2.h>
 #include <mm.h>
 
-extern pd_entry page_directory_[1024];
-
 void setup(u32 magic, u32 addr) {
   if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) printk("invalid magic number");
   if (addr & 7) printk("unaligned mbi");
 
-  printk("page_directory_[0]                : %d", page_directory_[0]);
-  printk("(pointer)page_directory_[0]       : 0x%x", &page_directory_[0]);
 
-
-  // MEMO
-  // MAGIC : 0x36d76289
   printk("grub info           0x%x", &addr);
-  printk("grub magic          0x%x", magic);
 
   // setup_heap();
   flush_screen();
@@ -29,7 +21,7 @@ void setup(u32 magic, u32 addr) {
   intr_init(); /* Initialize PIC */
   prot_init(); /* Initialize GDT, IDT, trap */
 
-  // init_paging(); /* Initialize paging (KERNEL_VIRTUAL_BASE - KERNEL_VIRTUAL_END) */
+  init_paging(); /* Initialize paging (KERNEL_VIRTUAL_BASE - KERNEL_VIRTUAL_END) */
 
   // setup_physical_memory(addr); /* fill 1 to physical memory */
   
--- mm/mmu.c
+++ mm/mmu.c
@@ -1,9 +1,8 @@
 #include <mm.h>
 #include <std/printk.h>
 
-// extern pd_entry page_directory[1024];
-extern pd_entry page_directory_[1024];
-pt_entry page_table[1024][1024];
+extern pd_entry page_directory[1024];
+static pt_entry page_table[1024][1024];
 
 u32 virt_to_phys(u32 *virt) {
   return (*virt & VM_ADDR_MASK);
@@ -17,22 +16,24 @@ void enable_paging() {
 
 /* TODO */
 void init_one_pde(u32 base_addr) {
-  // printk("PDE addr: 0x%x, index: %d", base_addr, pde_index(base_addr));
+  printk("PDE addr: 0x%x, index: %d", base_addr, pde_index(base_addr));
+
   int pde_index = pde_index(base_addr);
   for (int i=0;i<1024;i++) {
     u32 addr = base_addr+(i*sizeof(u32)*PTRS_PER_PTE);
-    page_table[pde_index][pte_index(addr)] = addr; // TODO
+    // TODO
+    page_table[pde_index][pte_index(addr)] = addr;
 
     /* --- for Debug ------- */
-    // if (i == 1023 || i == 0)
-    //   printk("  addr: 0x%x, index: %d, pde_index: %d, pte_index: %d", addr, i, pde_index, pte_index(addr));
+    if (i == 1023 || i == 0)
+      printk("  addr: 0x%x, index: %d, pde_index: %d, pte_index: %d", addr, i, pde_index, pte_index(addr));
     /* --------------------- */
   }
 
   /* --- for Debug ------- */
-  // u32 addr = base_addr+(1024*sizeof(u32)*PTRS_PER_PTE);
-  // printk("  addr: 0x%x, index: 1024 (next)", addr);
-  // printk("");
+  u32 addr = base_addr+(1024*sizeof(u32)*PTRS_PER_PTE);
+  printk("  addr: 0x%x, index: 1024 (next)", addr);
+  printk("");
   /* --------------------- */
 
   // TODO
@@ -40,16 +41,18 @@ void init_one_pde(u32 base_addr) {
 }
 
 /* mapping KERNEL_VIRTUAL_BASE - MEMORY_HIGH_LIMIT */
-void init_boot_paging() {
-  u32 *a = &page_directory_ - KERNEL_VIRTUAL_BASE;
-  a[0] = 1;
-  printk("page_directory_[0]                : %d", *a);
-  printk("page_directory_[0]                : %d", *a);
-  printk("(pointer)page_directory_[0]       : 0x%x", a);
+void init_paging() {
+  int pde_count = (MEMORY_HIGH_LIMIT - KERNEL_VIRTUAL_BASE) / (sizeof(u32)*PTRS_PER_PTE*PTRS_PER_PGD);
+  for (int i=0;i<=pde_count;i++) {
+    u32 addr = KERNEL_VIRTUAL_BASE+(i*(sizeof(u32)*PTRS_PER_PTE*PTRS_PER_PGD));
+    init_one_pde(addr);
+  }
 
-  // int pde_count = (MEMORY_HIGH_LIMIT - KERNEL_VIRTUAL_BASE) / (sizeof(u32)*PTRS_PER_PTE*PTRS_PER_PGD);
-  // for (int i=0;i<=pde_count;i++) {
-  //   u32 addr = KERNEL_VIRTUAL_BASE+(i*(sizeof(u32)*PTRS_PER_PTE*PTRS_PER_PGD));
-  //   // init_one_pde(addr);
-  // }
+  page_table[1023][1023];
+  // printk("pagetable 0x%x", page_table[1023][1023]);
+
+  /* TODO */
+  // write_cr3(page_directory);
+
+  printk("Setup paging... [OK]");
 }
