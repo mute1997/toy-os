f2f57c022c61a199691d70dd7374c18a8a415a2c
mute1997 <mute1008@gmail.com>
2018-06-18 15:49:44 +0900

    wip

--- mm/mmu.c
+++ mm/mmu.c
@@ -10,10 +10,22 @@ u32 virt_to_phys(u32 *virt) {
 
 /* map each 4KB page */
 void map_page(u32 *phys_addr, u32 *virt_addr, u32 size) {
+  u32 pd_index = pde_index((u32)virt_addr);
+  u32 pt_index = pte_index((u32)virt_addr);
 
-  // 10000011
-  // u32 pd = 0x0 | PRESENT | RW;
-  page_directory[pde_index(virt_addr)] = 0x0;
+  // MEMO: 
+  // Grub address = 0x1908280
+
+  /* 4MB paging*/
+  // page_directory[pd_index] = (((u32)phys_addr) & 0xFFC00000) | VM_PRESENT | VM_RW | VM_PS;
+
+  /* setting up page table entry */
+  page_table[pd_index][pt_index] = ((u32)phys_addr & 0xFFFFF000) |  VM_PRESENT | VM_RW;
+  
+  // TODO page_tableが正しく指定できてないっぽい
+  /* setting up page directory entry */
+  u32 *pt = &page_table[pd_index][pt_index];
+  page_directory[pd_index] = (((u32)pt) & 0xFFFFF000) | VM_PRESENT | VM_RW;
 }
 
 /* unmap page */
